eac3d5a901081792ffcaef9d49794ca50eaaa157
hnguzx <2544697696@qq.com>
2020-03-17 21:03:33 +0800

    希尔排序没有完成

--- src/cn/guzx/algorithm/sort/Demo.java
+++ src/cn/guzx/algorithm/sort/Demo.java
@@ -1,27 +1,43 @@
 package cn.guzx.algorithm.sort;
 
+import java.util.Date;
+
 public class Demo {
     public static void main(String[] args) {
-        int[] orgin = new int[8];
-        orgin[0] = 1;
-        orgin[1] = 22;
-        orgin[2] = 8;
-        orgin[3] = 7;
-        orgin[4] = 6;
-        orgin[5] = 5;
-        orgin[6] = 3;
-        orgin[7] = 2;
+        // int[] orgin = new int[100000];
+        // for (int i = 0; i < 100000; i++) {
+        // orgin[i] = (int) (Math.random() * 100000);
+        // }
+
+        int[] orgin = new int[10];
+        // for (int i = 8; i > 0; i--) {
+        //     orgin[8 - i] = i;
+        // }
+        orgin[0]= 8;
+        orgin[1]= 9;
+        orgin[2]= 1;
+        orgin[3]= 7;
+        orgin[4]= 2;
+        orgin[5]= 3;
+        orgin[6]= 5;
+        orgin[7]= 4;
+        orgin[8]= 6;
+        orgin[9]= 0;
 
         System.out.println("排序前：");
+        System.out.println(new Date());
         for (int i : orgin) {
             System.out.print(i + " ");
         }
-        System.out.println();
+        // System.out.println();
 
-        // bubb(orgin);
-        select(orgin);
+        // bubb(orgin); // 18s
+        // select(orgin); // 2s
+        // insert(orgin); // 4s
+        xier(orgin);
 
         System.out.println("排序后：");
+        System.out.println(new Date());
         for (int i : orgin) {
             System.out.print(i + " ");
         }
@@ -42,19 +58,57 @@ public class Demo {
     }
 
     public static void select(int[] orgin) {
-        int temp = 0;
+        int index;
+        int value;
+
         for (int i = 0; i < orgin.length; i++) {
-            temp = orgin[i];
-            for (int j = i; j < orgin.length; j++) {
-                if (orgin[j] < temp) {
-                    int  t = temp;
-                    temp = orgin[j];
-                    orgin[j] = t;
+            index = i;
+            value = orgin[i];
+            for (int j = i + 1; j < orgin.length; j++) {
+                if (orgin[j] < value) {
+                    index = j;
+                    value = orgin[j];
                 }
             }
-            orgin[i] = temp;
+            orgin[index] = orgin[i];
+            orgin[i] = value;
         }
 
     }
 
+    public static void insert(int[] orgin) {
+
+        int index;
+        int value;
+        for (int i = 1; i < orgin.length; i++) {
+            index = i - 1;
+            value = orgin[i];
+            while (index >= 0 && value < orgin[index]) {
+                orgin[index + 1] = orgin[index];
+                index--;
+            }
+            if (index != i) {
+                orgin[index + 1] = value;
+            }
+        }
+    }
+
+    public static void xier(int[] orgin) {
+        int increment = orgin.length / 2; // 增量
+        int index = 0;
+        int value = 0;
+        while (increment != 1) {
+            for (int i = 0; i < orgin.length / 2; i++) {
+                value = orgin[i];
+                index = i;
+                while (i + increment < orgin.length && orgin[index] > orgin[index + increment] ) {
+                    index = i + increment;
+                    value = orgin[i + increment];
+                }
+                orgin[i + increment] = orgin[i];
+                orgin[i] = value;
+            }
+            increment = increment / 2;
+        }
+    }
 }
